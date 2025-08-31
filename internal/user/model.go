0e30c9ba1aa1984640bb93c4b59551b99b14e68c
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2025-08-31 02:39:27 +0000

    feat: validate the fields

--- internal/user/model.go
+++ internal/user/model.go
@@ -1,14 +1,14 @@
 package user
 
 type Request struct {
-	DisplayName string `json:"display_name"`
-	Username    string `json:"username"`
-	Email       string `json:"email"`
-	Password    string `json:"password"`
+	DisplayName string `json:"display_name" binding:"required"`
+	Username    string `json:"username" binding:"required"`
+	Email       string `json:"email" binding:"required, email"`
+	Password    string `json:"password" binding:"required"`
 }
 
 type Response struct {
-	ID          string `json:"id"`
-	DisplayName string `json:"display_name"`
-	Username    string `json:"username"`
+	ID          string `json:"id" binding:"len=36"`
+	DisplayName string `json:"display_name" binding:"required"`
+	Username    string `json:"username" binding:"required"`
 }
